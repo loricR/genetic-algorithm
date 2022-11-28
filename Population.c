99e1da164914b614b7c8190cedce933dbd4c699c
Juloroti <louis.rolland2002@gmail.com>
2022-11-28 15:36:38 +0100

    initPop en C

--- Population.c
+++ Population.c
@@ -2,8 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-Population initPop(int taillePop) {
+Population ajoutt_pop(Population l, Individu i) {
 
+    Individus *temp;
+    temp = (Individus*)malloc(sizeof(Individus));
+    temp->val = i;
+    temp->next=l;
+
+    return temp;
+}
+
+Population initPop(int taillePop) {
+    if(taillePop == 0) {
+        return NULL;
+    } else {
+        return ajoutt_pop(initPop(taillePop-1),initIndiv_recursif(8));
+    }
 }
 
 Population triQualiteDec(Population p) {
