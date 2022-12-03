9747e549e45ca349fa7372afafbe18c0355650d0
Juloroti <louis.rolland2002@gmail.com>
2022-12-03 16:42:58 +0100

    Ajout des commentaires dans Individu.c et Population.c

--- Individu.c
+++ Individu.c
@@ -11,7 +11,9 @@ int vide(Individu l)
 
 void afficherInd(Individu l) {
     if(!vide(l)) {
+        // Tant qu'on est pas arrivés au bout de l'Individu
         while(l != NULL) {
+            // On affiche le bit et on passe au suivant
             printf("%d ",l->val);
             l = l->next;
         }
@@ -29,13 +31,15 @@ Individu ajoutq(Individu l, Bit v)
     temp->next = NULL;
     temp->val = v;
 
+    // Si la liste de bits n'est pas vide
     if (!vide(i))
     {
+        // On parcourt la liste de bits jusqu'à arriver au dernier élément
         while (i->next != NULL)
         {
             i = i->next;
         }
-
+        // Le dernier élément pointe vers la valeur attribuée à temp
         i->next = temp;
     } else
     {
@@ -50,6 +54,7 @@ Individu ajoutt(Individu l, Bit v)
     Bits *temp;
     temp = (Bits *) malloc(sizeof(Bits));
     temp->val = v;
+    // Le bit v pointe vers le premier élément de la liste de bits
     temp->next = l;
 
     return temp;
@@ -58,8 +63,10 @@ Individu ajoutt(Individu l, Bit v)
 Individu initIndiv(int longIndiv)
 {
     Individu l = NULL;
+    // Tant que la taille voulue de la liste de bits n'est pas atteinte
     while (longIndiv > 0)
     {
+        // On ajoute en queue de la liste de bits soit la valeur 0, soit 1
         l = ajoutq(l, rand() % 2);
         longIndiv--;
     }
@@ -68,10 +75,14 @@ Individu initIndiv(int longIndiv)
 
 Individu initIndiv_recursif(int longIndiv)
 {
+    // Cas d'arrêt : si on a atteint la taille voulue de la liste de bits
     if (longIndiv == 0)
     {
         return NULL;
-    } else
+    }
+    // Cas général : on ajoute en tête de la liste renvoyée par l'appel récursif de la
+    // fonction de taille voulue -1 soit la valeur 0, soit la valeur 1
+    else
     {
         return ajoutt(initIndiv_recursif(longIndiv - 1), rand() % 2);
     }
@@ -79,10 +90,13 @@ Individu initIndiv_recursif(int longIndiv)
 
 int decodeList(Individu l)
 {
+    // Cas d'arrêt : Si la liste de bits est vide
     if (vide(l))
     {
         return 0;
-    } else
+    }
+    // Cas général : On ajoute une puissance de 2 par bits parcouru
+    else
     {
         return (l->val + 2 * decodeList(l->next));
     }
@@ -90,8 +104,10 @@ int decodeList(Individu l)
 
 Individu croiserList(float pCroise, Individu l1, Individu l2)
 {
+    // Cas général
     if (!vide(l1) && !vide(l2))
     {
+        // Une chance sur 2 d'ajouter en tête de la liste résultat soit la valeur de l1, soit celle de l2
         if ((rand() % 20) <= pCroise * 10)   //TODO: voir si la proba est bonne
         {
             return ajoutt(croiserList(pCroise, l1->next, l2->next), l1->val);
@@ -100,6 +116,7 @@ Individu croiserList(float pCroise, Individu l1, Individu l2)
             return ajoutt(croiserList(pCroise, l1->next, l2->next), l2->val);
         }
     } else
+    // Cas d'arrêt : quand une des deux listes est vide
     {
         return NULL;
     }
@@ -108,15 +125,22 @@ Individu croiserList(float pCroise, Individu l1, Individu l2)
 float qualiteIndiv(Individu i)
 {
     return powf(((decodeList(i) / pow(2, longIndiv(i))) * 2 + 1), 2);
+    // Pour la manipulation :
+    // return -log10f(((decodeList(i) / pow(2, longIndiv(i))) * 4.9 + 0.1));
 }
 
 int longIndiv(Individu l)
 {
-    int compteur = 0;
+    /*int compteur = 0;
     while (!vide(l))
     {
         l = l->next;
         compteur++;
     }
-    return compteur;
+    return compteur;*/
+    
+    if (l != NULL)
+    {
+        return longIndiv(l->next) + 1;
+    } else return 0;
 }
